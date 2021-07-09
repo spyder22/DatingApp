8d5ba2f81c1271df1a0ddfbb4b11ca831b5265df
Kishan Saini <kishan8929@gmail.com>
2021-07-09 21:54:41 +0530

    end of section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -21,7 +21,12 @@ export class AppComponent implements OnInit{
   setCurrentUser()
   {
     const user:User= JSON.parse(localStorage.getItem('user') || '{}');
-    this.accountService.setCurrentUser(user);
+    if(user!=null && user.username!=null)
+    {
+      this.accountService.setCurrentUser(user);
+    }
+    
+    
   }
 
 
