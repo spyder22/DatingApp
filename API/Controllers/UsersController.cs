69c3a514b977fac6108e9caf9f4d7a5028078444
Kishan Saini <kishan8929@gmail.com>
2021-07-15 22:56:08 +0530

    End of Section 9

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -36,6 +36,7 @@ namespace API.Controllers
         [HttpGet("{username}")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
+            
             return await _userRepository.GetMemberAsync(username);
         }
 
