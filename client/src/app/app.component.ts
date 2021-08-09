b2fb39de270f7eb31aeb3a4f14a3b929e050db66
Kishan Saini <kishan8929@gmail.com>
2021-08-09 21:29:34 +0530

    End of section 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
+import { PresenceService } from './_services/presence.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent implements OnInit{
   title = 'The Dating App';
   users:any;
 
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService,private presenceService:PresenceService) {}
 
   ngOnInit(){
       this.setCurrentUser();
@@ -21,11 +22,11 @@ export class AppComponent implements OnInit{
   setCurrentUser()
   {
     const user:User= JSON.parse(localStorage.getItem('user') || '{}');
-    if(user!=null && user.username!=null)
+    if(user)
     {
       this.accountService.setCurrentUser(user);
+      this.presenceService.createHubConnection(user);
     }
-    
     
   }
 
